cf11d81d6994a7006b8af8962045ec71ecd0038d
iremyakincankaya <yakincanirem@gmail.com>
2024-12-29 21:05:35 +0300

    Add project files.

---
+++ Data/PatikaFirstDbContext.cs
@@ -0,0 +1,17 @@
+﻿using CodeFirstBasic.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CodeFirstBasic.Data
+{
+    public class PatikaFirstDbContext:DbContext
+    {
+        public DbSet<Movie>Movies { get; set; }
+        public DbSet<Game> Games { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=(localdb)\\MSSQLLocalDB;database=PatikaCodeFirstDb1;trusted_connection=true");
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
